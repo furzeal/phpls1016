2ae686abafd063b1ad20dae4d8f7c3b097bf7e1f
Maxim Shatalov <shatalov04@gmail.com>
2016-11-07 22:23:19 +0300

    #4.homework4 Ошибка метода post

--- homework4/php/insert_user.php
+++ homework4/php/insert_user.php
@@ -1,17 +1,19 @@
 <?php
 require_once 'connect.php';
 try {
+    echo $_POST;
     $name = htmlentities(strip_tags(trim($_POST['name'])), ENT_QUOTES);
     $age = (int)($_POST['age']);
     $description = htmlentities(strip_tags(trim($_POST['description'])), ENT_QUOTES);
     $login = htmlentities(strip_tags(trim($_POST['login'])), ENT_QUOTES);
     $password = htmlentities(strip_tags(trim($_POST['password'])), ENT_QUOTES);
-
+    echo $name,$age,$description,$login,$password;
     $sql = "INSERT INTO users(name, age, description, login, password)
                     VALUES (?, ?, ?, ?, ?)";
     $STH = $DBH->prepare($sql);
-    $STH->bindParam($name, $age, $description, $login, $password);
-    $STH->execute();
+    $data = [$name, $age, $description, $login, $password];
+    //var_dump($data);
+    $STH->execute($data);
 
     header('HTTP/1.1 307 Temporary Redirect');
     header('Location: homepage.php');
