c136424fc0ff93f99d2a3ad3c02c0590bcf5d337
Maxim Shatalov <shatalov04@gmail.com>
2016-11-13 22:14:32 +0300

    #4.homework4 Добавлено переименование файлов

---
+++ homework4/php/rename_file.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'connect.php';
+
+
+if (isset($_POST['newname'])) {
+
+    $newname = htmlentities(strip_tags(trim($_POST['newname'])), ENT_QUOTES);
+    $oldname = htmlentities(strip_tags(trim($_POST['oldname'])), ENT_QUOTES);
+    $oldpath = '../photos/' . $oldname;
+    $ext = strtolower(pathinfo($oldpath, PATHINFO_EXTENSION));
+    $newname .= ".$ext";
+    $newpath = '../photos/' . $newname;
+    //echo $oldname, $newname;
+    if (rename($oldpath, $newpath)) {
+        // rename reference from DB
+        $sql = "UPDATE users
+                SET filename = ?
+                WHERE filename = ?";
+        $data = [$newname, $oldname];
+        $STH = $DBH->prepare($sql);
+        $STH->execute($data);
+    }
+
+    header('HTTP/1.1 307 Temporary Redirect');
+    header('Location: homepage.php');
+}
+
+?>
+<html>
+<body>
+<form method='post'> <!--action="homepage.php" >-->
+    <div>
+        <label for="newname">Введите новое имя файла</label>
+        <div><input type="text" name="newname" id="newname"></div>
+    </div>
+    <div>
+        <div><input type="hidden" name="oldname" id="oldname"
+                    value="<?php echo $_GET['name']; ?>"></div>
+    </div>
+    <div>
+        <input type="submit" value="Переименовать">
+    </div>
+</form>
+</body>
+</html>
